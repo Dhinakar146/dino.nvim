0086043f09b03d123164ebdc5b040df157d29ea1
V Dhinakar <dhinakar.v@mahindra.com>
2024-05-25 12:09:21 +0530

    proper working nvim setup

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,12 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+local map = vim.keymap.set
+
+map("n", "<C-d>", "<C-d>zz", { desc = "Center the window when moving down" })
+map("n", "<C-u>", "<C-u>zz", { desc = "Center the window when moving up" })
+map("n", "n", "nzzzv", { desc = "Center the window when going to next find result" })
+map("n", "N", "Nzzzv", { desc = "Center the window when going to previous find result" })
+map("n", "<leader>x", ":bd<cr>", { desc = "Close current buffer" })
+map("n", "<leader>y", [["+y]], { desc = "Copy to clipboard" })
+map("v", "<leader>y", [["+y]], { desc = "Copy to clipboard" })
---
+++ lua/config/options.lua
@@ -0,0 +1,27 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+-- LSP Server to use for Python.
+-- Set to "basedpyright" to use basedpyright instead of pyright.
+vim.g.lazyvim_python_lsp = "pyright"
+-- vim.g.lazyvim_python_ruff = "ruff_lsp"
+
+vim.opt.encoding = "utf-8" -- set encoding
+vim.opt.nu = true -- enable line numbers
+vim.opt.relativenumber = true -- relative line numbers
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true -- convert tabs to spaces
+vim.opt.autoindent = true -- auto indentation
+vim.opt.list = true -- show tab characters and trailing whitespace
+
+vim.opt.ignorecase = true -- ignore case when searching
+vim.opt.smartcase = true -- unless capital letter in search
+
+vim.opt.hlsearch = false -- do not highlight all matches on previous search pattern
+vim.opt.incsearch = true -- incrementally highlight searches as you type
+
+vim.opt.termguicolors = true -- enable true color support
