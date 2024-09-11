b0a051ce2a649249c90b74208c5833965e166485
V Dhinakar <dhinakar.v@mahindra.com>
2024-09-11 22:15:11 +0530

    fixed undo dir path

--- lua/theprimeagen/set.lua
+++ lua/theprimeagen/set.lua
@@ -14,7 +14,7 @@ vim.opt.wrap = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undodir = vim.fn.expand('~/AppData/Local/nvim-data/undo')
 vim.opt.undofile = true
 
 vim.opt.hlsearch = false
